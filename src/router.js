936541d813de594ca51e5d9a83652edc61a6edca
Dineen <35275339+dineensch@users.noreply.github.com>
2019-02-05 17:08:41 -0800

    upgraded to cli3

---
+++ src/router.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import Router from "vue-router";
+import CitySearch from "@/views/CitySearch";
+import CurrentWeather from "@/views/CurrentWeather";
+import Forecast from "@/views/Forecast";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "CitySearch",
+      component: CitySearch
+    },
+    {
+      path: "/:cityId/current",
+      name: "CurrentWeather",
+      component: CurrentWeather
+    },
+    {
+      path: "/:cityId/forecast",
+      name: "Forecast",
+      component: Forecast
+    }
+  ]
+});
